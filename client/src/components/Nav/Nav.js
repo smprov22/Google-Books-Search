4c6389b59dabf362e74f4907f092b2a16269466d
smprov22 <smprovence22@gmail.com>
2019-07-28 14:59:01 -0700

    initial app created and navbar

---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css";
+
+function Nav() {
+  return (
+    <div className="navbar-fixed">
+      <nav>
+        <div className="nav-wrapper container">
+          <a href="/" className="brand-logo">Google Books</a>
+          <ul className="right">
+            <li><a href="/">Search</a></li>
+            <li><a href="/">Saved</a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Nav;
